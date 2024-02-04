d66dc3fdfdcef24c475727a3d9b5f39466fed441
Patrick odey <patrickodey419@hmail.com>
2024-02-04 15:53:07 +0100

    initial commit

---
+++ pages/_app.js
@@ -0,0 +1,30 @@
+import "../styles/globals.css";
+import { storyblokInit, apiPlugin } from "@storyblok/react";
+import Feature from "../components/Feature";
+import Grid from "../components/Grid";
+import Page from "../components/Page";
+import Teaser from "../components/Teaser";
+import Hero from "../components/Hero";
+
+const components = {
+  feature: Feature,
+  grid: Grid,
+  teaser: Teaser,
+  page: Page,
+  hero: Hero
+};
+
+storyblokInit({
+  accessToken: "ZscLgizcb3ryzjxfYOPiuwtt",
+  use: [apiPlugin],
+  components,
+  apiOptions: {
+    region: ''
+  }
+});
+
+function MyApp({ Component, pageProps }) {
+  return <Component {...pageProps} />;
+}
+
+export default MyApp;
