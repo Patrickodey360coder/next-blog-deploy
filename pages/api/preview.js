eed501d671b9cc6fa8b11f34fefdb8c80308740c
Patrick odey <patrickodey419@hmail.com>
2024-02-24 17:48:35 +0100

    deploy

--- pages/api/preview.js
+++ pages/api/preview.js
@@ -22,7 +22,7 @@ export default async function preview(req, res) {
 
 
 
-  if ( req.query.secret !== NEXT_PUBLIC_STORYBLOK_ACCESS_TOKEN) {
+  if ( req.query.secret !== "NEXT_PUBLIC_STORYBLOK_ACCESS_TOKEN") {
     return res.status(401).json({ message: 'Invalid token' })
   }
 
