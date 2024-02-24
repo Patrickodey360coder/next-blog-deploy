193220610e8888336fd3be0278b27fa3ccd10e32
Patrick odey <patrickodey419@hmail.com>
2024-02-24 17:01:33 +0100

    deploy

--- pages/api/preview.js
+++ pages/api/preview.js
@@ -15,14 +15,20 @@
 //   res.redirect(`/${slug}?${params[1]}`);
 // }
 
-export default function handler(req, res) {
+export default async function preview(req, res) {
   const { slug = '' } = req.query;
 
   const params = req.url.split('?');
 
-  if ( req.query.secret !== "ZeBQnfmciPsQuDshMshgwwtt") {
+  const secret = process.env.NEXT_PUBLIC_STORYBLOK_PREVIEW;
+
+  if ( req.query.secret !== secret) {
     return res.status(401).json({ message: 'Invalid token' })
   }
+
+  console.log(secret)
+
+  con
 
   res.setPreviewData({});
 
